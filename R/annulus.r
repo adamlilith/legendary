b962c5e94199482ecc8429bf0562454b92f4f1ac
Adam B. Smith <adam@earthskysea.org>
2018-11-08 20:43:23 -0600

    Update documentation

--- R/annulus.r
+++ R/annulus.r
@@ -1,18 +1,18 @@
 #' Plot an annulus on an existing plot.
 #'
-#' This function adds an annulus to an existingg plot.
+#' This function adds an annulus to an existing plot.
 #' @param x Numeric, x-coordinate of center of annulus.
 #' @param y Numeric, y-coordinate of center of annulus.
 #' @param inner Numeric, inner radius.
 #' @param outer Numeric, outer radius.
 #' @param deg Two-element numeric list, degrees across which to draw the annulus (0 is "north" and increasing values go clockwise). Default is \code{c(0, 360)}.
 #' @param n Integer, number of vertices used to approximate a circle.
-#' @param col Character or integer, color used to fill annulus.  Note that if \code{col} and \code{border} are different colors and \code{deg} is \code{c(0, 360)} (the default), then you wil abel to see the line connecting the inner and outer borders.
-#' @param border Character or integer, color used to draw annulus border.  Note that if \code{col} and \code{border} are different colors and \code{deg} is \code{c(0, 360)} (the default), then you wil abel to see the line connecting the inner and outer borders.
+#' @param col Character or integer, color used to fill annulus.  Note that if \code{col} and \code{border} are different colors and \code{deg} is \code{c(0, 360)} (the default), then you will able to see the line connecting the inner and outer borders.
+#' @param border Character or integer, color used to draw annulus border.  Note that if \code{col} and \code{border} are different colors and \code{deg} is \code{c(0, 360)} (the default), then you will able to see the line connecting the inner and outer borders.
 #' @param force0 Logical, if \code{TRUE} then negative values of inner and outer are coerced to equal 0. If \code{FALSE} then throws an error.
-#' @param ... Arguments to send to \code{\link{graphics::polygon}}.
+#' @param ... Arguments to send to \code{\link[graphics]{polygon}}.
 #' @return Nothing (side-effect is to add an annulus to an existing plot).
-#' @seealso \code{\link{graphics::polygon}}
+#' @seealso \code{\link[graphics]{polygon}}
 #' @examples
 #' plot(0, 0, xlim=c(-1, 1), ylim=c(-1, 1))
 #' annulus(x=0.3, y=-0.2, inner=1, outer=0.6, col='cornflowerblue')
